b16c5354147dfd07ad174660304abb5a2b0e3d08
Bogdan Chioaru <bogdan.chioaru@ebs.msft>
2017-10-25 14:50:16 +0300

    2

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-var restify = require('restify');
-
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
-
+var restify = require('restify'); 
 var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+
+var users = {};
+var max_user_id = 0;
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.bodyParser());
+
+server.get('/', (req, res, next) => {
+  res.setHeader('content-type', 'application/json');
+  res.writeHead(200);
+  res.end(JSON.stringify(users));
+  return next();
+});
+
+server.post('/user', (req, res, next) => {
+  var user = req.params;
+  max_user_id++;
+  user.id = max_user_id;
+  users[user.id] = user
+  
+  res.setHeader('content-type', 'application/json');
+  res.writeHead(200);
+  res.end(JSON.stringify(user));
+
+  return next();
+});
 
 server.listen(8081, function() {
   console.log('%s listening at %s', server.name, server.url);
-});
+}); 
